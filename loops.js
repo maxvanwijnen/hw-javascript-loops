ef73158e14e8471134d0f0c34233462b827c6430
maxvanwijnen <maxvanwijnen@gmail.com>
2022-06-16 16:49:02 +0200

    opdracht3

--- loops.js
+++ loops.js
@@ -21,7 +21,14 @@ for (let i = 0; i < 3; i++) {
 // klaar!
  // ==========================================
 
-
+for (let i = 0; i < 5 ; i++) {
+    if (i < 4) {
+        console.log('loop');
+    }
+    else {
+        console.log('klaar!');
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -32,6 +39,18 @@ for (let i = 0; i < 3; i++) {
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+
+for (let i = 0; i <= 15 ; i++) {
+    let evenOrOdd = null;
+    if (i % 2 == 0){
+        evenOrOdd = 'even';
+    }
+    else {
+        evenOrOdd = 'oneven';
+    }
+    console.log(i + ' is ' + evenOrOdd);
+}
+
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
